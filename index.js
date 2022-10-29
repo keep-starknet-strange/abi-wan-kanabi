900e5f9c7e17a59285fec67491a93002f26c106e
ivpavici <ivan.pavicic@live.com>
2022-10-30 00:25:44 +0200

    git-ignore

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.call = void 0;
-function call(abi, f, args) {
-    throw new Error('todo');
-}
-exports.call = call;
