6ec929bab139496cb72e5fd455ff43b27cb8b63f
Thomas Zamojski <thomas.zamojski@quadratic-labs.com>
2023-01-23 12:34:45 +0100

    feat: transpile ABI json to d.ts file
    Currently, the abi json is inserted manually into the typescript file with const assertion.
    The objective was to import it as const. However, this turns out to be impossible in typescript at the moment.
    We introduce the script test/extract_abi.sh as an intermediary step that generates a .d.ts file from the .json abi.
    
    See Issue #4 for more information.

---
+++ test/extract_abi.sh
@@ -0,0 +1,16 @@
+# Extract typescript declarations from ABIs JSON files
+
+# Arguments:
+#    variadic number of arguments are input json files
+#    last argument is the output directory
+#
+# Example:
+#    extract_abi.sh abi*.json ./
+
+for f in ${@:1:$#-1}; do
+    basename=${f##*/}
+    name=${basename%.*}
+    echo $basename
+    echo $name
+    echo -E "declare const abi: $(cat $f) as const; export default abi;" > ${name}.d.ts
+done;
